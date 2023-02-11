4e3741758a041710487d43b4dbb0ad19f3b8f7fe
roneya <iamrohanvidhate@gmail.com>
2023-02-11 15:46:53 +0530

    last

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -12,8 +12,8 @@ import java.util.List;
 @Service
 public class OrderService {
 
-    @Autowired
-    OrderRepository orderRepository;
+
+    OrderRepository orderRepository = new OrderRepository();
 
     public void addOrder(Order order)
     {
